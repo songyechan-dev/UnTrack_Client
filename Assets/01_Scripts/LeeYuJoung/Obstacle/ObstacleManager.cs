3d05f6b0d96fa9d913fba5f9bb01e70a5be2f120
LeeYuJung <105534978+LeeYuJoung@users.noreply.github.com>
2024-01-15 11:17:47 +0900

    Test

--- Assets/01_Scripts/LeeYuJoung/Obstacle/ObstacleManager.cs
+++ Assets/01_Scripts/LeeYuJoung/Obstacle/ObstacleManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON;
 
 public class ObstacleManager : MonoBehaviour
 {
+
+
+    // 현재 Obstacle을 플레이어가 작업 중인지 확인
+    public bool isWorking = false;
+
     // 시작
     void Start()
     {
@@ -14,5 +20,10 @@ public class ObstacleManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ObstacleWork(string equipment)
+    {
+
     }
 }
