0662311a8813b8518bc20d005778904e614447aa
LeeYuJung <105534978+LeeYuJoung@users.noreply.github.com>
2024-01-17 15:49:26 +0900

    Upgrade

--- Assets/01_Scripts/LeeYuJoung/Upgrade/UpgradeManager.cs
+++ Assets/01_Scripts/LeeYuJoung/Upgrade/UpgradeManager.cs
@@ -19,14 +19,40 @@ public class UpgradeManager : MonoBehaviour
     }
 
     // 엔진 업그레이드
+    public void UpgradeEngine()
+    {
+        if(StateManager.Instance().voltNum >= engineUpgradePrice)
+        {
 
+        }
+        else
+        {
+            Debug.Log(":::: 볼트가 부족 업그레이드 실패 ::::");
+        }
+    }
 
     // 저장소 업그레이드
+    public void UpgradeStorage()
+    {
+        if (StateManager.Instance().voltNum >= storageUpgradePrice)
+        {
 
+        }
+        else
+        {
+            Debug.Log(":::: 볼트가 부족 업그레이드 실패 ::::");
+        }
+    }
 
-    // 기계 업그레이드
+    // 기계 업그레이드 → 업그레이드하고 싶은 기계 선택
+    public void UpgradeMachine(GameObject _gameObject)
+    {
 
+    }
 
     // 기계 추가 구매
+    public void BuyMachine(GameObject _gameObject)
+    {
 
+    }
 }
