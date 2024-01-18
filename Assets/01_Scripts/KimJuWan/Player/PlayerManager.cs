b34e877233b622f290c0a511ac9d8304ad61aa10
Kimjw <rlawndhskwlq@gmail.com>
2024-01-18 11:03:07 +0900

    0117/Player

--- Assets/01_Scripts/KimJuWan/Player/PlayerManager.cs
+++ Assets/01_Scripts/KimJuWan/Player/PlayerManager.cs
@@ -6,20 +6,20 @@ public class PlayerManager : MonoBehaviour
 {
     
     PlayerController playerController;
-    
+    ItemManager itemManager;
     
 
 
     private void Start()
     {
         playerController = GetComponent<PlayerController>();
-        
+        itemManager = GameObject.FindGameObjectWithTag("Item").GetComponent<ItemManager>();
     }
 
     // 업데이트
     void FixedUpdate()
     {
-        CollectIngredient();
+        
 
         
     }
@@ -27,33 +27,45 @@ public class PlayerManager : MonoBehaviour
     //플레이어의 정면에 장애물이 있는 경우 파괴
     public void CollectIngredient()
     {
+        
         RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 1f))
+        int layerMask = (1 << LayerMask.NameToLayer("IgnoreRaycast"));
+        layerMask = ~layerMask;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3f, layerMask))
         {
             GameObject hitObject = hit.collider.gameObject;
-
-            if (hitObject.CompareTag("Obstacle"))
-            {
-                hitObject.GetComponent<MeshRenderer>().material.color = Color.red;
-            }
-            else if (hitObject.CompareTag("Item"))
-            {
-                //테스트
-                playerController.nearObject = hitObject ;
-                playerController.isPick = true;
-                
-            }
-            else if (hitObject.CompareTag("Factory"))
-            {
-
-            }
-            else if (hitObject.CompareTag("DroppedTrack"))
-            {
-
-            }
             
-          
+            switch (hitObject.tag)
+            {
+                case "Obstacle":
+                    hitObject.GetComponent<MeshRenderer>().material.color = Color.red;
+                    break;
+                case "Item":
+                    if (!playerController.isPick)
+                    {
+                        playerController.isPick = true;
+                        
+                       
+                    }
+                    if(playerController.isPick)
+                    {
+                        playerController.nearObject = hitObject;
+                        playerController.items.Push(hitObject);
+                        hitObject.transform.position = hitObject.transform.position;
+                        hitObject.layer = 2;
+                        hitObject.GetComponent<BoxCollider>().enabled = false;
+                           
+                            
+                    }
+                    break;
+                case "Factory":
+                    break;
+                case "DroppedTrack":
+                    break;
+                default:
+                    
+                    break;
+            }
         }
     }
 
