df1d0e6730c32e4b415ab711a0c3a40a79758a2d
Kimjw <rlawndhskwlq@gmail.com>
2024-01-19 18:32:28 +0900

    0119 PlayerComplete

--- Assets/01_Scripts/KimJuWan/Player/PlayerController.cs
+++ Assets/01_Scripts/KimJuWan/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using Google.GData.Documents;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using System.Collections;
@@ -6,37 +7,50 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Jobs;
 
+
+//Player 애니메이션용 STATE
+public enum PLAYERSTATE
+{
+    IDLE = 0,
+    WALK,
+    PICKUP,
+    DROP,
+    EQUIP_AX,
+    EQUIP_PICK,
+    DYNAMITE,
+    BUCKET
+
+}
 public class PlayerController : MonoBehaviour
 {
-    public enum PLAYERSTATE
-    {
-        IDLE = 0,
-        WALK,
-        PICKUP,
-        DROP,
-        AX,
-        PICK
-    }
+    
     private float moveSpeed = 6f;
     
     Vector3 moveDirection;
     Rigidbody rb;
     PlayerManager playerManager;
-    //public InventoryManager inventoryManager; 
+     
     public bool isPick;
 
     public float currentTime = 0f;
-    public float spaceTime = 1.5f;
+    public float spaceTime = .2f;
 
-
+    public int keyCode = 0;
+    public List<KeyCode> keys = new List<KeyCode>()
+    {
+        KeyCode.Space,
+        KeyCode.LeftControl,
+        KeyCode.LeftShift
+    };
 
 
     // 시작
+   
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+       
         playerManager = GetComponent<PlayerManager>();
-        
+        rb = GetComponent<Rigidbody>();
         
     }
 
@@ -44,20 +58,28 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         PlayerMove();
-        //TODO 김주완 0118: Space 키 변수화 하기(단축키 설정)
-        if (Input.GetButton("Pick"))
+        //TODO 김주완 0118: Space 키 변수화 하기(단축키 설정) -> 0119 완료
+        
+
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKey(keys[keyCode]))
         {
             currentTime += Time.deltaTime;
         }
-        if (Input.GetButtonUp("Pick"))
+        if (Input.GetKeyUp(keys[keyCode]))
         {
             playerManager.CollectIngredient();
             currentTime = 0;
         }
 
-        
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    playerManager.ButtonClick();
+        //}
     }
-
     //플레이어 이동
     public void PlayerMove()
     {
@@ -69,8 +91,13 @@ public class PlayerController : MonoBehaviour
         moveDirection =  moveDirection.normalized * moveSpeed * Time.deltaTime;
         if (!(h == 0 && v == 0))
         {
-            rb.MovePosition(transform.position + moveDirection);
+            transform.position +=moveDirection;
             transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;  
         }
         
     }
--- Assets/01_Scripts/KimJuWan/UI/UIManager.cs
+++ Assets/01_Scripts/KimJuWan/UI/UIManager.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public PlayerController playerController;
     // 시작
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // 업데이트
@@ -17,13 +18,12 @@ public class UIManager : MonoBehaviour
         
     }
 
-    void UIRayCast()
+    public void ChangeKeyCode()
     {
-        RaycastHit hit;
-
-        if(Physics.Raycast(transform.position, transform.up, out hit))
+        playerController.keyCode++;
+        if(playerController.keyCode >2)
         {
-
+            playerController.keyCode = 0;
         }
     }
 }
