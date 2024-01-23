5b75901a94fd587ec2f9fb49442420b60cc5b9c7
Kimjw <rlawndhskwlq@gmail.com>
2024-01-23 17:11:00 +0900

    GameEndScene

--- Assets/01_Scripts/SongYeChan/PlayableButton/PlayableButtonInfo.cs
+++ Assets/01_Scripts/SongYeChan/PlayableButton/PlayableButtonInfo.cs
@@ -10,7 +10,10 @@ public class PlayableButtonInfo : MonoBehaviour
         GAME_START = 0,
         GAME_EXIT = 1,
         RANKING = 2,
-        SETTING = 3
+        SETTING = 3,
+        FIND_ROOM_02 = 4,
+        BACK_TO_MAIN_02 = 5,
+        GAME_START_02 = 6,
     }
     public Info myInfo;
 }
