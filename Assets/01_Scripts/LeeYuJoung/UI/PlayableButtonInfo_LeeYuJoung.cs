702a57c3186918ea48679af2545c6c1b0037cc60
LeeYuJung <105534978+LeeYuJoung@users.noreply.github.com>
2024-01-24 16:48:50 +0900

    UpgradeScene Change

--- Assets/01_Scripts/LeeYuJoung/UI/PlayableButtonInfo_LeeYuJoung.cs
+++ Assets/01_Scripts/LeeYuJoung/UI/PlayableButtonInfo_LeeYuJoung.cs
@@ -10,10 +10,14 @@ public class PlayableButtonInfo_LeeYuJoung : MonoBehaviour
         ENGINE_UPGRADE_04 = 1,
         STORAGE_UPGRADE_04 = 2,
         MACHINE_UPGRADE_04 = 3,
-        PRODUCTIONMACHINE_BUY_04 = 4,
-        DYNAMITEMACHINE_BUY_04 = 5,
-        WATERTANK_BUY_04 = 6,
-        GAME_EXIT_04 = 7
+        PRODUCTIONMACHINE_UPGRADE_04 = 4,
+        DYNAMITEMACHINE_UPGRADE_04 = 5,
+        WATERTANK_UPGRADE_04 = 6,
+        PRODUCTIONMACHINE_BUY_04 = 7,
+        DYNAMITEMACHINE_BUY_04 = 8,
+        WATERTANK_BUY_04 = 9,
+        GAME_EXIT_04 = 10
     }
     public Info myInfo;
+    public int machineUpgradeIDX = 0;
 }
