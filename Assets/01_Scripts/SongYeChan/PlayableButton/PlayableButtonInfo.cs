e92a8e2998cc5118c85b1bff46ee50c53f927c1f
LeeYuJung <105534978+LeeYuJoung@users.noreply.github.com>
2024-02-02 17:24:48 +0900

    UIManager

--- Assets/01_Scripts/SongYeChan/PlayableButton/PlayableButtonInfo.cs
+++ Assets/01_Scripts/SongYeChan/PlayableButton/PlayableButtonInfo.cs
@@ -14,8 +14,21 @@ public class PlayableButtonInfo : MonoBehaviour
 
         GAME_START_02 = 4,
         BACK_02 = 5,
-        FIND_ROOM_02 = 6
+        FIND_ROOM_02 = 6,
+
+        CONTINUE_04 = 7,
+        ENGINE_UPGRADE_04 = 8,
+        STORAGE_UPGRADE_04 = 9,
+        MACHINE_UPGRADE_04 = 10,
+        PRODUCTIONMACHINE_UPGRADE_04 = 11,
+        DYNAMITEMACHINE_UPGRADE_04 = 12,
+        WATERTANK_UPGRADE_04 = 13,
+        PRODUCTIONMACHINE_BUY_04 = 14,
+        DYNAMITEMACHINE_BUY_04 = 15,
+        WATERTANK_BUY_04 = 16,
+        GAME_EXIT_04 = 17
 
     }
     public Info myInfo;
+    public int machineUpgradeIDX = 0;
 }
